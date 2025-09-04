ae6574adcf91c726a79ca0de4b7c59341cc9abd7
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-04 17:37:26 +0200

    fix: Code

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
 def print_last_digit(number):
-
-    abs = abs(number)
-    last_dig = abs % 10
-
+    absolu = abs(number)
+    last_dig = absolu % 10
     print("{}".format(last_dig), end="")
-
     return last_dig
