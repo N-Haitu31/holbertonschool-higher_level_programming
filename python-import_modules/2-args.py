a65a88ade8bf151d162acc915ab75454ecc60007
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-05 10:24:38 +0200

    feat: Add comments to explain

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,16 +3,17 @@
 from sys import argv
 
 if __name__ == "__main__":
-    count = len(argv) - 1
+    count = len(argv) - 1 # Compte les arguments, exclut le nom du script 'argv[0]'
 
+    # Vérifiez le nombre d'arguments
     if count == 0:
-        print("0 arguments.")
+        print("0 arguments.") # Affiche si aucun argument n'est passé
 
     elif count == 1:
-        print("1 argument:")
-        print("1: {}".format(argv[1]))
+        print("1 argument:") # Affiche pour un seul argument
+        print("1: {}".format(argv[1])) # Affiche l'argument avec sa position
 
     else:
-        print("{} arguments:".format(count))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(count)) # Affiche le nombre d'arguments
+        for i in range(1, len(argv)): # Commencez à l'index 1 pour ignorer le nom du script
+            print("{}: {}".format(i, argv[i])) # Affiche chaque argument avec sa position
