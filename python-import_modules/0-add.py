c242fa2c7c2dce6921d5186516177958961240d0
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-05 09:44:40 +0200

    feat: Add comments to explain

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-from add_0 import add
+from add_0 import add # Importe la fonction 'add' du module 'add_0'
 a = 1
 b = 2
-if __name__ == "__main__":
-    add(a, b)
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__": # Vérifie si le script est exécuté directement
+    add(a, b) # Appelle la fonction 'add' avec 'a' et 'b' comme arguments
+    print("{} + {} = {}".format(a, b, add(a, b))) # Affiche le résultat de l'addition dans un format lisible
