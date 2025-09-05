e3f2f96d75f71a4a2a857773b16b9dfff5200849
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-05 10:38:01 +0200

    feat: Add comments to explain

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,11 +3,11 @@
 from sys import argv
 
 if __name__ == "__main__":
-    count = len(argv) - 1
+    count = len(argv) - 1 # Compte le nombre d'arguments, exclut le nom du script
     if count == 0:
         print("0")  # Affiche '0' si aucun argument n'est passé
     else:
         total = 0
         for i in argv[1:]:  # Parcourt tous les arguments à partir de argv[1]
-            total += int(i)
+            total += int(i) # Convertit chaque argument en entier et l'ajoute à total
         print(total)  # Affiche le total de la somme
