06a2cfd29ae55b5de6301c457bdf3562a21c27e1
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-08 13:50:31 +0200

    feat: Add print_matrix_integer function

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    for row in matrix:
+        for index, element in enumerate(row):
+            print("{:d}".format(element), end="")
+
+            if index != len(row) - 1:
+                print(" ", end="")
+
+        print()
