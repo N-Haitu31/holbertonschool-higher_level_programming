b878d146e8f1e6088d893186ea375bb527882506
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-09 13:38:15 +0200

    feat: Add uniq_add to sum unique integers in a list.

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+
+    total = 0
+    unique_numbers = []
+
+    for i in my_list:
+        if i not in unique_numbers:
+            unique_numbers.append(i)
+            total += i
+
+    return total
