d398de177c26c9ca0221eead963a7def4ffb48ba
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-15 14:40:26 +0200

    feat: Add Square class with size validation via properties

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines a Square class with a private size attribute"""
+
+
+class Square:
+    """Represents a square with a private size"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square using
+        the property setter"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+
+        return self.__size ** 2
