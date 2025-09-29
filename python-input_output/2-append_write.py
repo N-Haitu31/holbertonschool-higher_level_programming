2cd8b89be80e89b3db1a974d068145c413045e25
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-09-29 12:02:55 +0200

    feat: Implement append_write for file appending

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module provides a function to write a string to a UTF8 text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Appends a string at the end of a text
+    file (UTF8) and returns the number of characters added.
+
+    Args:
+        filename (str): The name of the file.
+        text (str): The text to append.
+
+    Returns:
+        int: Number of characters added.
+    """
+
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+
+    return len(text)
