2c892b82dc71045e9c3dda1421962959c078a3e5
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-10-26 11:33:01 +0100

    https://gemini.google.com/app/2b18fbe496daa771?hl=fr#:~:text=feat%3A%20Add%20script%20to%20search%20MySQL%20states%20by%20name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # 1. Get the four command-line arguments
+    user = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # 2. Connect to the MySQL database
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=user,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    # 3. Create a cursor object
+    cur = conn.cursor()
+
+    # 4. Execute a query using string formatting with format()
+    # We use BINARY to make the comparison case-sensitive
+    query = (
+        "SELECT * FROM states "
+        "WHERE BINARY name = '{}' "
+        "ORDER BY id ASC"
+    ).format(state_name)
+
+    # 5. Fetch and display results
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # 6. Clean close of the cursor and connection
+    cur.close()
+    conn.close()
